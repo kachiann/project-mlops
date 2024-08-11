7a85f2d8f1e11406d4e7ca8a1ef68d7d415a5321
Onyekachi (Osisiogu) Emenike <dotteanna62@gmail.com>
2024-08-12 00:10:12 +0200

    fix

--- tests/unit-test/create_bike_data.py
+++ tests/unit-test/create_bike_data.py
@@ -1,6 +1,5 @@
 import base64
 import json
-
 from utils import BIKE_DATA_TEMPLATE
 
 # Convert the data to a JSON string
@@ -10,7 +9,7 @@ json_str = json.dumps(BIKE_DATA_TEMPLATE)
 base64_str = base64.b64encode(json_str.encode("utf-8")).decode("utf-8")
 
 # Specify the path for the output file
-file_path = "bike_data.b64"  
+file_path = "bike_data.b64"
 
 # Write the base64 string to a file
 with open(file_path, "w", encoding="utf-8") as f:  # Use "w" mode to write
