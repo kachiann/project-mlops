f19ca24a0f9646e63da9b973e3734ac6df8668ee
Onyekachi (Osisiogu) Emenike <dotteanna62@gmail.com>
2024-08-12 16:50:16 +0200

    sys fix

--- src/experiment_tracking.py
+++ src/experiment_tracking.py
@@ -17,10 +17,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
-
+from constants import FEATURES
 # Local application imports
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from constants import FEATURES
+
 
 # Set the remote tracking URI
 REMOTE_TRACKING_URI = "http://127.0.0.1:5000"
